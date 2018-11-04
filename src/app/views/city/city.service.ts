c499a75d9b17bec267563a983642e8744d0539bd
Tushar Jagdale <tjagdale@dminc.com>
2018-11-05 00:55:26 +0530

    test

--- src/app/views/city/city.service.ts
+++ src/app/views/city/city.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import { GlobalSettings } from '../../shared/globalsettings';
+
 @Injectable()
 export class CityService {
   constructor(private _http: Http) { }
